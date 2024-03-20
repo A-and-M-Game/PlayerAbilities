5bd10ff70dca5251b37bb85e3104a6def0e2f2fb
A-and-M-Game <michael.simutis@icloud.com>
2024-03-20 18:23:15 -0400

    Initial commit

--- src/main/java/net/michael/playerabilities/PlayerAbilities.java
+++ src/main/java/net/michael/playerabilities/PlayerAbilities.java
@@ -8,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.bernie.geckolib.GeckoLib;
 
-//Test Comment
+//Test Comment 2
 public class PlayerAbilities implements ModInitializer {
 	public static final String MOD_ID = "playerabilities";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
